04590ffa790aff3e81b5ff7fef26ec3024d648ed
eltrul <105537798+eltrul@users.noreply.github.com>
2024-08-02 18:38:43 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+"use strict"; 
+
+const config = require("./config.js"); 
+
+const chalk = require("chalk"); 
+
+var mongoConnection = require("./database/mongoConnection"); 
+var mongoConnection = new mongoConnection(config.mongo.url);
+
+const util = require("./utilly"); 
+
+const routeInitalizer = require("./api/routeInitalizer"); 
+
+routeInitalizer(mongoConnection); 
