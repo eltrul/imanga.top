224c5d5b29adf99044ad51d94e6f76156ee80e59
Karyl <62021442+lenlenlL6@users.noreply.github.com>
2024-08-02 18:55:28 +0700

    update

---
+++ routes/api/auth.js
@@ -0,0 +1,18 @@
+const route = require("express").Router(); 
+
+route.use(bodyParser.json());
+route.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+/*
+Request id:
+1    REGISTER
+2    LOGIN
+3    FORGOT PASSWORD
+*/
+
+
+route.post("/authorize", function(req, res) {
+    res.send(req.body);
+});
