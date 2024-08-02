d8f11b5e737c85dd4713996d1a78cb88e9f65338
Karyl <62021442+lenlenlL6@users.noreply.github.com>
2024-08-02 20:26:33 +0700

    Update auth.js

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,21 @@
-const route = require("express").Router(); 
+const route = require("express").Router();
+const mysql = require("mysql");
 
 route.use(bodyParser.json());
 route.use(bodyParser.urlencoded({
     extended: true
 }));
+
+// Connect to mysql.
+const conn = mysql.createConnection({
+    host: "mysql-368dfd1c-duyvipka2008-2026.e.aivencloud.com",
+    port: 25983,
+    user: "avnadmin",
+    password: "AVNS_5M7T-PNu1r1STRbyXoA",
+    database: "defaultdb"
+});
+conn.connect();
+conn.query("CREATE TABLE IF NOT EXISTS user(id INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(50) NOT NULL, password VARCHAR(50) NOT NULL, email VARCHAR(80) NOT NULL)");
 
 
 /*
@@ -11,18 +23,29 @@ Request id:
 1    REGISTER
 2    LOGIN
 3    FORGOT PASSWORD
+
+Response id:
+1    LOGIN
+2    WRONG USERNAME OR PASSWORD
 */
 
 
 route.post("/authorize", function(req, res) {
-    let requestId = req.body.id;
-    if(requestId == 1) {
-        route.send("Register");
+    const body = req.body;
+    if (body.requestId != 2) {
         return;
     }
-    if(requestId == 2) {
-        route.send("Login");
-        return;
-    }
-    route.send("Forget Password");
+    const username = body.username;
+    const password = body.password;
+    conn.query('SELECT * FROM user WHERE username = "' + username + '" OR email = "' + username + '"', function(error, result, fields) {
+        if(result.length > 0) {
+            if(result.password != password) {
+                route.send(2);
+                return;
+            }
+            route.send(1);
+            return;
+        }
+        route.send(2);
+    });
 });
