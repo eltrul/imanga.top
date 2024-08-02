4617d9853236690ed8d58b77f9496ce8f516cdf5
eltrul <105537798+eltrul@users.noreply.github.com>
2024-08-02 21:34:18 +0700

    Update mongoConnection.js

--- database/mongoConnection.js
+++ database/mongoConnection.js
@@ -1,1 +1,28 @@
+const mongoose = require("mongoose"); 
 
+const chalk = require("chalk"); 
+
+const log = console.log;
+
+const model = require("./Model.js');
+module.exports = class mongoConnection
+  {
+    __constructor(url) 
+    {
+      mongoose.connect(url)
+      .then(() => log(chalk.green("[+] Connected to mongodb services")))
+      .catch((err) => { 
+        log(chalk.red("[!] Failed to connect to mongodb"));
+        log(err);
+        process.exit();
+      }) 
+      this.model = new model(mongoose);
+    }
+
+    function Models() 
+    {
+      return this.model
+    }
+  }
+
+                    
