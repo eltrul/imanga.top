74f6f153e706abe4402fe23b1492d0968eeeba93
eltrul <105537798+eltrul@users.noreply.github.com>
2024-08-02 19:34:59 +0700

    Update RouteInitalizer.js

---
+++ routes/RouteInitalizer.js
@@ -0,0 +1,42 @@
+"use strict"; 
+
+const express = require("express"); 
+
+const app = express(); 
+
+const bodyparser = require("body-parser"); 
+const session = require('express-session');
+
+const memorystore = require('memorystore')(session); 
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({
+	extended: true
+}));
+
+app.use(session({
+    secret: 'try to guess that',
+    resave: false,
+    store: new memorystore({
+    checkPeriod: 86400000 
+    }),
+    saveUninitialized: false,
+    cookie: { secure: false }
+  })) 
+
+
+
+const fs = require("node:fs"); 
+const path = require("node:path"); 
+
+
+const chalk = require("chalk"); 
+
+const log = console.log; 
+
+log(chalk.yellow("[+] Initalizing Routes")); 
+
+module.exports = function(db) 
+  {
+    
+  }
