93ace3b603e1e61f435f9e70a238cf10da705e6a
eltrul <105537798+eltrul@users.noreply.github.com>
2024-08-02 18:56:26 +0700

    Update utilly.js

--- utilly.js
+++ utilly.js
@@ -5,7 +5,50 @@ const path = require("node:path");
 
 var result = {}; 
 
-class F
+// wtf am i doing? :sob:
+class FileManager 
+{
+    __construct(fileName) 
+    {
+        this.name = fileName; 
+    }
+
+    function getFullPath() 
+    {
+        return path.join(__dirname, this.name); 
+    }
+
+    function read() 
+    {
+        this.content = fs.readFile(getFullPath());
+        return this;
+    } 
+
+    function content() 
+    {
+        return this.content; 
+    } 
+
+    function toJson() 
+    {
+        try 
+        {
+            return JSON.parse(this.content); 
+        } 
+        catch 
+        {
+            return false;
+        }
+    }
+
+    function write(content) 
+    {
+        fs.writeFile(this.getFullPath(), content); 
+
+    }
+
+    function replace(content) 
+}
 
 function result.readFile(filename)
 {
