7101dbcf1a9ee2d1f976cfb0269ed3aef9913598
eltrul <105537798+eltrul@users.noreply.github.com>
2024-08-03 08:11:40 +0700

    Update userFunction.js

--- functions/userFunction.js
+++ functions/userFunction.js
@@ -7,9 +7,8 @@ module.exports = class userFunction
 
             async function findUser(filter) 
             {
-                var result = await this.db.query("select * from users where ? = ?", [filter]); 
-                if (result.length == 0) return false; 
-                this.user = result[0]; 
+                this.db.findOne(filter)
+                if (!this.db) return false; 
                 return this;
             }
 
@@ -20,25 +19,8 @@ module.exports = class userFunction
 
             async function update(data) 
             {
-                var parsed_data = [this.user.id];
-                var cache = [];
-                Object.keys(data).forEach(key => {
-                    parsed_data.push(key) ;
-                    cache.push(data[key]);
-                });
-                parsed_data.concat(cache); 
-
-                var wtfamidoing = ""; 
-                for (i=0, i < parsed_data.length/2, i++) 
- 
-
-                     {     
-                     i2 = (i == 0) ? "" : " ";  
-                     i3 = i < parsed_data.length/2 ? "," : "";
-                     wtfamidoing = wtfamidoing + i2 + "? = ?" + i3
-                }
-               
-
-                return await this.db.query(`update users where id = ? set ${wtfamidoing}`, parsed_data); 
+                    return this.db.findOneAndUpdate({id: this.user.id},data);
             } 
+
+            async function 
         }
