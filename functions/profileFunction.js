77d6a8befd8bd4cb600550ccca474ad5a6a0aaa2
eltrul <105537798+eltrul@users.noreply.github.com>
2024-08-03 09:02:10 +0700

    Update profileFunction.js

--- functions/profileFunction.js
+++ functions/profileFunction.js
@@ -1,1 +1,19 @@
 
+module.exports = class profile 
+  {
+      __constructor(db) 
+      {
+          this.db = db.Models().User;
+      }
+
+      async function findUser(filter) 
+            {
+                this.db.findOne(filter)
+                if (!this.db) return false; 
+                return this;
+            } 
+
+      async function changeProfilePicture(picture) 
+      {
+      }
+  }
